9663ae148e5d2bb810851484249b49bcb5ec30aa
Kevin Gonzalez <kevin.gonzalez@devoteam.lu>
2018-11-02 16:15:06 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# floatsoft.github.io
-Floatsoft GitHub Page
---
+++ precache-manifest.da8be127008be2c22a84e028b93297ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47490a778058521cabac",
+    "url": "/static/css/main.214d405b.chunk.css"
+  },
+  {
+    "revision": "47490a778058521cabac",
+    "url": "/static/js/main.47490a77.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/react-logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c5b2a78a7962a6a46e05b754c559d63d",
+    "url": "/index.html"
+  }
+];
